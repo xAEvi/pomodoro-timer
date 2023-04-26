75bf7e10a55c553e5342db2defe877c6d68d51c2
xaevi <xeavi02@hotmail.com>
2023-04-26 10:15:45 -0500

    Added controls

---
+++ src/components/Controls.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import '../stylesheets/Controls.css'
+
+const Controls = ({ }) => {
+  return (
+    <div className="controls-container">
+      <div className="control session">
+        <h3 id="session-label">Session Length:</h3>
+        <input
+          id="session-length"
+          type="number"
+          value={25}
+        />
+      </div>
+      <div className="control break">
+        <h3 id="break-label">Break Length:</h3>
+        <input
+          id="break-legnth"
+          type="number"
+          value={5}
+        />
+      </div>
+      <div className="control long-break">
+        <h3 id="long-break-label">Long Break Length:</h3>
+        <input
+          id="long-break-length"
+          type="number"
+          value={15}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Controls;
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import tomato from '../images/tomato.png'
 import '../stylesheets/Timer.css'
 
-const Timer = props => {
-    return (
-        <div className="container">
-            <img className="image" src={tomato}></img>
-            <h1 className="timer">00:00</h1>
-        </div>
-    );
+const Timer = ({ }) => {
+  return (
+    <div className="timer-container">
+       <img className="image" src={tomato}></img>
+      <h1 id="time-left" className="timer">00:00</h1>
+    </div>
+  );
 }
 
 export default Timer;
