81981802270784d9ef068aad6d318b158e7d74d0
xaevi <xeavi02@hotmail.com>
2023-04-26 09:25:07 -0500

    First Commit

---
+++ src/components/Timer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import tomato from '../images/tomato.png'
+
+const Timer = props => {
+    return (
+        <div>
+            <img src={tomato}></img>
+        </div>
+    );
+}
+
+export default Timer;
