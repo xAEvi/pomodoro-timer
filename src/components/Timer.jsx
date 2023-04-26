dc75041bda93698a3aaf6c30c50c85f88d0aaa18
xaevi <xeavi02@hotmail.com>
2023-04-26 09:33:16 -0500

    Added Timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import tomato from '../images/tomato.png'
+import '../stylesheets/Timer.css'
 
 const Timer = props => {
     return (
-        <div>
-            <img src={tomato}></img>
+        <div className="container">
+            <img className="image" src={tomato}></img>
+            <h1 className="timer">00:00</h1>
         </div>
     );
 }
